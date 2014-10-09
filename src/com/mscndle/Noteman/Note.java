6006fd5653b2101f0f63593783111340eb7b6a5a
Mandeep Condle <mandeepcondle@gmail.com>
2014-10-08 21:16:11 -0400

    updating layouts and fragments

--- src/com/mscndle/Noteman/Note.java
+++ src/com/mscndle/Noteman/Note.java
@@ -1,6 +1,66 @@
 package com.mscndle.Noteman;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+//
+// Note class that contains all critical information about a note
+//
 public class Note {
 
+    private UUID mId;
+    private boolean mSynced; // to check if it should be updated / newly created in the cloud
+    private Date mDate;
+    private String mTitle;
+    private String mContents;
+    private Set<String> mTags;
 
+    public Note() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+        mTags = new HashSet<String>();
+    }
+
+
+    public Set<String> getmTags() {
+        return mTags;
+    }
+
+    public void setmTags(Set<String> mTags) {
+        this.mTags = mTags;
+    }
+
+    public String getmContents() {
+        return mContents;
+    }
+
+    public void setmContents(String mContents) {
+        this.mContents = mContents;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public boolean ismSynced() {
+        return mSynced;
+    }
+
+    public void setmSynced(boolean mSynced) {
+        this.mSynced = mSynced;
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
 }
--- src/com/mscndle/Noteman/NotesList.java
+++ src/com/mscndle/Noteman/NotesList.java
@@ -1,4 +1,46 @@
 package com.mscndle.Noteman;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+//
+//  Data structure to store the user's notebook
+//  Using a singleton since it will be instantiated only once
+//
 public class NotesList {
+
+    private static NotesList sNotesList;
+    private Context mAppContext;
+    private ArrayList<Note> mNotesList;
+
+    private NotesList(Context appContext) {
+        mAppContext = appContext;
+        mNotesList = new ArrayList<Note>();
+    }
+
+    //
+    //  Checks if an object already exits, if not, uses the Application Context
+    //  to create another one
+    //
+    public static NotesList get(Context c) {
+        if (sNotesList == null) {
+            sNotesList = new NotesList(c.getApplicationContext());
+        }
+        return sNotesList;
+    }
+
+    public ArrayList<Note> getNotes() {
+        return mNotesList;
+    }
+
+    public Note getNote(UUID id) {
+        for (Note n : mNotesList) {
+            if (n.getmId().equals(id)) {
+                return n;
+            }
+        }
+        return null;
+    }
 }
